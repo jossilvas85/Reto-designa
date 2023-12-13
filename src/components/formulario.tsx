89e12ead718ee846c7f5eb9ae499f163042ef1ff
jossilvas85 <jossilvas85@gmail.com>
2023-12-13 02:14:25 -0600

    campos de formulario completos (diseño)

--- src/components/formulario.tsx
+++ src/components/formulario.tsx
@@ -45,7 +45,7 @@ const Formulario = () => {
         <>
             <h1 className="my-5">Section</h1>
 
-            <div className="form-card text-start p-5">
+            <div className="form-card text-start p-5 container">
                 {/* Form */}
                 <form>
                     {/* Text */}
@@ -80,22 +80,6 @@ const Formulario = () => {
                         </div>
                     </div>
 
-                    {/* List */}
-                    <div className="mb-3">
-                        <label className="form-label fw-bold">List input</label>
-                        <input
-                            id=""
-                            name=""
-                            type="number"
-                            min={1}
-                            max={10}
-                            className="form-control form-input"
-                        ></input>
-                        <div className="form-text fw-bold">
-                            Sample description
-                        </div>
-                    </div>
-
                     {/* Select */}
                     <div className="mb-3">
                         <label className="form-label fw-bold">
@@ -113,6 +97,107 @@ const Formulario = () => {
                             <option value="2">Two</option>
                             <option value="3">Three</option>
                         </select>
+                    </div>
+
+                    {/* Table */}
+                    <div className="my-5">
+                        <label className="form-label fw-bold">Table</label>
+                        <div className="row p-3 my-3 border-bottom border-3 border-secondary">
+                            <label className="align-self-center fw-bold col-9">
+                                Text
+                            </label>
+                            <select
+                                id=""
+                                name=""
+                                className="form-select form-input col"
+                            >
+                                <option hidden defaultValue={''}>
+                                    Selecciona una opción
+                                </option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    {/* List */}
+                    <div className="my-5">
+                        <label className="form-label fw-bold">List</label>
+                        <div className="row p-3 my-3">
+                            <label className="align-self-center fw-bold col-10">
+                                Text
+                            </label>
+                            <select
+                                id=""
+                                name=""
+                                className="form-select form-input col"
+                            >
+                                <option hidden defaultValue={''}></option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                            </select>
+                        </div>
+                        <div className="row p-3 my-3">
+                            <label className="align-self-center fw-bold col-10">
+                                Text
+                            </label>
+                            <select
+                                id=""
+                                name=""
+                                className="form-select form-input col"
+                            >
+                                <option hidden defaultValue={''}></option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                            </select>
+                        </div>
+                        <div className="row p-3 my-3 border-bottom border-3 border-secondary">
+                            <label className="align-self-center fw-bold col-10">
+                                Text
+                            </label>
+                            <select
+                                id=""
+                                name=""
+                                className="form-select form-input col"
+                            >
+                                <option hidden defaultValue={''}></option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    {/* Email */}
+                    <div className="mb-3">
+                        <label className="form-label fw-bold">Email</label>
+                        <input
+                            id=""
+                            name=""
+                            type="email"
+                            className="form-control form-input"
+                        ></input>
+                        <div className="form-text fw-bold">
+                            Ingresa el correo
+                        </div>
+                    </div>
+
+                    {/* Password */}
+                    <div className="mb-3">
+                        <label className="form-label fw-bold">Password</label>
+                        <input
+                            id=""
+                            name=""
+                            type="password"
+                            min={10}
+                            className="form-control form-input"
+                        ></input>
+                        <div className="form-text fw-bold">
+                            La contraseña tiene que tener al menos x caracteres
+                        </div>
                     </div>
 
                     {/* Checkbox */}
